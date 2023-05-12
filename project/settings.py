64fb9b404df0de7c98cce3aed514bf42ac3937c9
Asmahasnath1 <133239295+Asmahasnath1@users.noreply.github.com>
2023-05-12 16:17:20 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':'asmahasnath$default',
+        'HOST': 'asmahasnath.mysql.pythonanywhere-services.com',
+        'USER': 'asmahasnath',
+        'PASSWORD': 'Asm@1425',
     }
 }
 
